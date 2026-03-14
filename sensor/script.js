efc27718df4e82cbe8dc193a797a0e7a293e1e1b
Abigail-Ch <al234021@alumnos.uacj.mx>
2026-03-14 22:46:16 +0000

    Update script.js

--- sensor/script.js
+++ sensor/script.js
@@ -1,1 +1,43 @@
+function login(){
+
+let user="admin";
+let pass="1234";
+
+let u=document.getElementById("usuario").value;
+let p=document.getElementById("password").value;
+
+if(u==user && p==pass){
+
+document.getElementById("login").style.display="none";
+document.getElementById("sistema").style.display="block";
+
+}else{
+
+document.getElementById("error").innerHTML="Usuario incorrecto";
+
+}
+
+}
+
+function actualizar(){
+
+let humedad = document.getElementById("humedad").value;
+let temp = document.getElementById("temp").value;
+
+let ledH = document.getElementById("ledH");
+let ledT = document.getElementById("ledT");
+
+if(humedad <= 8){
+    ledH.style.background = "red";
+}else{
+    ledH.style.background = "green";
+}
+
+if(temp <= 25){
+    ledT.style.background = "blue";
+}else{
+    ledT.style.background = "orange";
+}
+
+}
 
