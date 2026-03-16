b89a33530fe1cb8c5570c5759a79b0f0ffc77f41
Abigail-Ch <al234021@alumnos.uacj.mx>
2026-03-15 22:25:05 -0600

    Update script.js

--- sensor/script.js
+++ sensor/script.js
@@ -78,64 +78,43 @@ let normales=0;
 let advertencia=0;
 let criticos=0;
 
-
-/* HUMEDAD */
-
 let humedad=document.getElementById("humedad").value;
 
 if(humedad <=30){
-
 criticos++;
-
 }else if(humedad <=60){
-
 advertencia++;
-
 }else{
-
 normales++;
-
 }
-
-
-/* PROXIMIDAD */
 
 let distancia=document.getElementById("distancia").value;
 
 if(distancia <=2){
-
 criticos++;
-
 }else if(distancia <=5){
-
 advertencia++;
-
 }else if(distancia>5){
-
 normales++;
-
 }
-
-
-/* ACTUALIZAR PANEL */
 
 document.getElementById("normales").innerHTML=normales;
 document.getElementById("advertencia").innerHTML=advertencia;
 document.getElementById("criticos").innerHTML=criticos;
 
-
-/* ALERTA */
-
 let alerta=document.getElementById("alertaCritica");
 
 if(criticos>0){
-
 alerta.style.display="flex";
-
 }else{
-
 alerta.style.display="none";
+}
 
 }
+
+
+function cerrarAlerta(){
+
+document.getElementById("alertaCritica").style.display="none";
 
 }
